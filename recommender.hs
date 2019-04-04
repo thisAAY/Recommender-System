ae7bf6592fbfee3128ca82ef7d2fbcef31a69755
Ahmed Ali <ahmedaliaay@gmail.com>
2019-04-04 20:41:29 +0200

    Create getAllUsersStats function

--- recommender.hs
+++ recommender.hs
@@ -11,7 +11,7 @@ items = [ "suit " , "dress" , "shoes" , "T−shirt" , "Jacket" , "skirt" , "shor
  "shirt" , "trousers" , "scarf" , "mp3 player" , "TV" , "LCD screen" , "headphone" , "laptop" , "keyboard" , "mouse" , "cellphone" , "headphone" , "earphone" , "milk" ,"cheese" , "bread" , "chocolate" , "meat" ,"flour" , "sugar" ,"oil" ,"tomatoes" , "chicken" ,"yogurt" , "cereal" , "beans" , "fool","eggs" ]
 
 purchases = [ ( "A" , [[ "dress" , "shoes" ] , [ "milk" , "cheese" , "eggs"]]) ,
- ("B" , [[ "earphone" , "mouse" , "laptop" ] , [ " mp3 player" ] ] ) ,
+ ("B" , [[ "earphone" , "mouse" , "laptop" ] , [ "mp3 player" ] ] ) ,
  ("C" , [ [ "bread" , "milk" ] , [ "shoes" ] ] ) ,
  ("D" , [ [ "milk" , "meat" , "chicken" , " yogurt" ] , [ "beans" , "cereal" , "flour" , "sugar"] , [ "tomatoes" , "oil" , "chicken" ] ] ) ,("E" , [ ] )]
 
@@ -20,3 +20,21 @@ purchases = [ ( "A" , [[ "dress" , "shoes" ] , [ "milk" , "cheese" , "eggs"]]) ,
 
 getList user ps = if fst (head ps) == user then snd (head ps) else getList user (tail ps)
 createEmptyFreqList xs = [ (x,[]) | x <- xs]
+
+countOcc item list  = sum [1 | a <- list ,a == item ]
+getItemFrequency user item = countOcc item (concat (getList user purchases))
+getUserStats user = [(user,b,(getItemFrequency user b)) | b <- items]
+getAllUsersStats = [(user,getUserStats user) | user <- users]
+
+
+
+
+
+
+
+
+
+
+
+
+
