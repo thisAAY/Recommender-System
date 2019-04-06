5804b7f1661d3d8430460024e09307bb36bd9679
Ahmed Ali <ahmedaliaay@gmail.com>
2019-04-06 12:14:23 +0200

    Change the output format of getUsersAllStats

--- recommender.hs
+++ recommender.hs
@@ -7,13 +7,14 @@ randomZeroToX x= unsafePerformIO (getStdRandom (randomR (0, x)))
 --Names
 
 users = [ "A" , "B" , "C" , "D" , "E" ]
-items = [ "suit " , "dress" , "shoes" , "T−shirt" , "Jacket" , "skirt" , "shorts" ,
+items = [ "suit" , "dress" , "shoes" , "T−shirt" , "Jacket" , "skirt" , "shorts" ,
  "shirt" , "trousers" , "scarf" , "mp3 player" , "TV" , "LCD screen" , "headphone" , "laptop" , "keyboard" , "mouse" , "cellphone" , "headphone" , "earphone" , "milk" ,"cheese" , "bread" , "chocolate" , "meat" ,"flour" , "sugar" ,"oil" ,"tomatoes" , "chicken" ,"yogurt" , "cereal" , "beans" , "fool","eggs" ]
 
 purchases = [ ( "A" , [[ "dress" , "shoes" ] , [ "milk" , "cheese" , "eggs"]]) ,
- ("B" , [[ "earphone" , "mouse" , "laptop" ] , [ "mp3 player" ] ] ) ,
- ("C" , [ [ "bread" , "milk" ] , [ "shoes" ] ] ) ,
- ("D" , [ [ "milk" , "meat" , "chicken" , " yogurt" ] , [ "beans" , "cereal" , "flour" , "sugar"] , [ "tomatoes" , "oil" , "chicken" ] ] ) ,("E" , [ ] )]
+              ("B" , [[ "earphone" , "mouse" , "laptop" ] , [ "mp3 player" ] ] ) ,
+              ("C" , [ [ "bread" , "milk" ] , [ "shoes" ] ] ) ,
+              ("D" , [ [ "milk" , "meat" , "chicken" , " yogurt" ] , [ "beans" , "cereal" , "flour" , "sugar"] , [ "tomatoes" , "oil" , "chicken" ] ] ) ,
+              ("E" , [] )]
 
 
 
@@ -22,10 +23,22 @@ getList user ps = if fst (head ps) == user then snd (head ps) else getList user 
 createEmptyFreqList xs = [ (x,[]) | x <- xs]
 
 countOcc item list  = sum [1 | a <- list ,a == item ]
-getItemFrequency user item = countOcc item (concat (getList user purchases))
-getUserStats user = [(user,b,(getItemFrequency user b)) | b <- items]
-getAllUsersStats = [(user,getUserStats user) | user <- users]
+flattenPurcahes user  = concat (getList user purchases)
 
+removeItem _ []                 = []
+removeItem x (y:ys) | x == y    = removeItem x ys
+                    | otherwise = y : removeItem x ys
+
+getItemCart _ [] = []
+getItemCart item carts = if (elem item (head carts))
+                                 then (removeItem item (head carts)) 
+                                 else if (length carts == 1) then [] 
+                                 else  getItemCart item (tail carts)
+
+getUserStats user = [(item,[(a,countOcc a (getItemCart item (getList user purchases)) ) 
+                    | a <- getItemCart item (getList user purchases)]) | item <- items]
+
+getAllUsersStats = [(user,getUserStats user) | user <- users]
 
 
 
